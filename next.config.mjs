54651bb98b04737d8b50dce652eb0f21250f51ba
SeptAlfauzan <alfauzansepta@gmail.com>
2024-09-20 18:30:51 +0700

    update: homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,31 @@
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+import {PrismaPlugin} from '@prisma/nextjs-monorepo-workaround-plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-    },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.plugins = [...config.plugins, new PrismaPlugin()]
-        }
-        // Enable WebAssembly
-        config.experiments = {
-            ...config.experiments,
-            asyncWebAssembly: true,
-        };
+	experimental: {
+		serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+	},
+	webpack(config, {isServer}) {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
 
-        // Optionally, if you're using Wasm in Web Workers
-        config.output = {
-            ...config.output,
-            webassemblyModuleFilename: 'static/wasm/[modulehash].wasm'
-        };
+		// Enable WebAssembly
+		config.experiments = {
+			...config.experiments,
+			asyncWebAssembly: true,
+		};
 
-        return config;
-    },
+		// Optionally, if you're using Wasm in Web Workers
+		config.output = {
+			...config.output,
+			webassemblyModuleFilename: 'static/wasm/[modulehash].wasm',
+		};
+
+		return config;
+	},
+	images: {
+		domains: ['images.unsplash.com'],
+	},
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,46 @@
 "use client";
-import { Box, Button, Text } from "@chakra-ui/react";
-import Image from "next/image";
+import { Box, Button, Text, Image, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+  return <HomePage/>
+}
+
+
+function HomePage(){
   const router = useRouter();
   return (
-    <Box
-      display={"flex"}
-      flexDir={"column"}
-      alignItems={"center"}
-      height={"100vh"}
-      padding={4}
-      gap={4}
-    >
-      <Button alignSelf={"flex-end"} onClick={() => router.push("/auth")}>
-        <Text>Login</Text>
-      </Button>
-      <Box
-        width={"100%"}
-        height={"80vh"}
-        position={"relative"}
-        borderRadius={12}
-        overflow={"clip"}
-        background={"black"}
-      >
-        <Image
-          src="/please_wait.jpg"
-          alt="landing"
-          layout="fill"
-          objectFit="contain"
-          priority
-        />
+    <Box px={{md: '124px', base: 4}} position="relative" display="flex" flexDir="column">
+      <Box pt="24px" position="sticky" width="100%" display="flex" flexDir="row">
+        <Text fontWeight="bold">Oh My REST API</Text>
+        <Button ml="auto" onClick={() => router.push("/auth")}>
+          <Text>Login</Text>
+        </Button>
       </Box>
-      <Text
-        paddingX={4}
-        background={"black"}
-        color={"yellow"}
-        display={{ base: "block", md: "none" }}
-      >
-        Wanna try Oh-My-REST?
-      </Text>
-      <Text
-        paddingX={4}
-        background={"black"}
-        color={"yellow"}
-        display={{ base: "block", md: "none" }}
-      >
-        Click login button on right corner
-      </Text>
-    </Box>
+      <BlurBackground/>
+      <Box display="flex" minHeight="80vh" alignItems="center">
+        <Box flex={1}>
+          <Heading fontSize={52} fontWeight="bold">
+              Oh My REST API: Supercharge your App Development Workflow
+          </Heading>
+           <Text fontSize={24} mb={12} mt={4}>
+            Craft Custom Dummy APIs in Minutes, Not Days
+          </Text>
+          <Button onClick={() => router.push("/register")} background="black" color="white" _hover={{background: "gray.600", color: "white"}}>Create new account now</Button>
+        </Box>
+        <Box flex={1} position="relative">
+          <Image boxShadow="2xl" rounded="lg" width={624} height={360} alt="preview" src="https://i.imgur.com/aQYafG5.png"/>
+          </Box>
+        </Box>
+      </Box>
   );
+}
+
+function BlurBackground(){
+  return (
+          <Box position="absolute" display="flex" left={"40%"} top={"25%"}>
+            <Box borderRadius="50%" backgroundColor="blue" width="240px" height="120px" filter={`blur(120px)`}></Box>
+            <Box borderRadius="50%" backgroundColor="red" width="240px" height="240px" filter={`blur(120px)`}></Box>
+          </Box>
+         );
 }
